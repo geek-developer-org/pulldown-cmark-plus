311b42a1c5cb1008479f23560d4a1874a200d38a
geek-developer-org <84998510+geek-developer-org@users.noreply.github.com>
2023-12-24 11:33:10 +0900

    modified: src/md_utils.rs

--- src/md_utils.rs
+++ src/md_utils.rs
@@ -8,31 +8,7 @@ pub fn to_html(markdown: &str) -> String {
             let cowstr = cowstr.as_ref().to_owned();
             let mut codeblock = String::from("");
 
-            let mut lang: &str = "";
-            let mut file: &str = "";
-            for part in cowstr.split(',').collect::<Vec<&str>>() {
-                if !lang.is_empty() && !file.is_empty() {
-                    break;
-                }
-                if let Some(pos) = part.find("=") {
-                    if file.is_empty() {
-                        let (key, val) = (part[..pos].trim(), part[pos + 1..].trim());
-                        if key == "file" && !val.is_empty() {
-                            file = val;
-                        }
-                    }
-                } else if lang.is_empty()
-                    && !part.trim().is_empty()
-                    && part.trim() != "linenostart"
-                    && part.trim() != "linenos"
-                    && part.trim() != "hl_lines"
-                    && part.trim() != "hide_lines"
-                    && part.trim() != "file"
-                    && part.trim() != "no_style"
-                {
-                    lang = part.trim();
-                }
-            }
+            let (lang, file, _) = get_lang(&cowstr);
 
             if !file.is_empty() {
                 codeblock.push_str("<div class=\"codeblock-file\"><span>");
@@ -43,7 +19,7 @@ pub fn to_html(markdown: &str) -> String {
             if !lang.is_empty() {
                 codeblock.push_str(" class=\"");
                 codeblock.push_str(lang);
-                codeblock.push_str(" language-");
+                codeblock.push_str("\" data-lang=\"language-");
                 codeblock.push_str(lang);
                 codeblock.push_str("\"");
             }
@@ -60,8 +36,65 @@ pub fn to_html(markdown: &str) -> String {
     buf
 }
 
+fn get_lang(cowstr: &str) -> (&str, &str, bool) {
+    let parts = match cowstr.contains(":") {
+        true => cowstr.split(':').collect::<Vec<&str>>(),
+        _ => cowstr.split(',').collect::<Vec<&str>>(),
+    };
+    if cowstr.contains(":") {
+        return (
+            match parts.get(0) {
+                Some(s) => s.trim(),
+                None => "",
+            },
+            match parts.get(1) {
+                Some(s) => s.trim(),
+                None => "",
+            },
+            match parts.get(2) {
+                Some(s) => s.trim().to_lowercase() == "true",
+                None => false,
+            },
+        );
+    }
+
+    let mut lang: &str = "";
+    let mut file: &str = "";
+    let mut no_style = false;
+    for part in parts {
+        if !lang.is_empty() && !file.is_empty() && no_style {
+            break;
+        }
+        if let Some(pos) = part.find("=") {
+            let (key, val) = (part[..pos].trim(), part[pos + 1..].trim());
+            if lang.is_empty() && key.trim() == "lang" && !val.trim().is_empty() {
+                lang = val.trim();
+            }
+            if file.is_empty() && key.trim() == "file" && !val.trim().is_empty() {
+                file = val.trim();
+            }
+            if !no_style && key.trim() == "no_style" && !val.trim().is_empty() {
+                no_style = val.trim() == "true";
+            }
+        } else if lang.is_empty()
+            && !part.trim().is_empty()
+            && part.trim() != "linenostart"
+            && part.trim() != "linenos"
+            && part.trim() != "hl_lines"
+            && part.trim() != "hide_lines"
+            && part.trim() != "file"
+            && part.trim() != "no_style"
+        {
+            lang = part.trim();
+        }
+    }
+
+    (lang, file, no_style)
+}
+
 #[allow(dead_code)]
 pub fn get_title(markdown: &str) -> String {
+    let mut i: usize = 0;
     for line in markdown.lines().collect::<Vec<&str>>() {
         if line.starts_with("#") {
             let title = line.trim_start_matches('#').trim();
@@ -69,6 +102,10 @@ pub fn get_title(markdown: &str) -> String {
             if !title.is_empty() {
                 return title.to_string();
             }
+        }
+        i += 1;
+        if i > 30 {
+            break;
         }
     }
     "".to_string()
