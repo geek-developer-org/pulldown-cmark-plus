4facdb8bec754a54c8e600b7bf3441e9167c984a
geek-developer-org <84998510+geek-developer-org@users.noreply.github.com>
2023-12-27 00:03:12 +0900

    modified static css

--- static/css/minify.sh
+++ static/css/minify.sh
@@ -1,10 +1,1 @@
-cat github-markdown.css custom-style.css>.temp
-cleancss -o github-markdown.min.css .temp
-
-cat github-markdown-light.css custom-style-light.css>.temp
-cleancss -o github-markdown-light.min.css .temp
-
-cat github-markdown-dark.css custom-style-dark.css>.temp
-cleancss -o github-markdown-dark.min.css .temp
-
-rm .temp
+cleancss -o markdown.min.css markdown.css
